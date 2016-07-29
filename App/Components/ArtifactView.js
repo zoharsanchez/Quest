72761a90d769288b0a6db9c376ef73ea58abbac3
zoharsanchez <lorena.zohar@gmail.com>
2016-07-28 21:10:53 -0700

    Add view componenet for each individual artifact

--- App/Components/ArtifactView.js
+++ App/Components/ArtifactView.js
@@ -20,19 +20,49 @@ const styles = StyleSheet.create({
   imageContainer: {
     margin: 3,
     flex: 2,
-    backgroundColor: 'powderblue',
     alignItems: 'stretch'
   },
   contentContainer: {
-    margin: 3,
+    margin: 2,
     flex: 3,
     justifyContent: 'center'
   },
-  listImage: {
-    height: 75
+  artifactViewImage: {
+    flex: 1,
+    resizeMode: 'contain'
   },
-  listText: {
-    fontSize: 17
+  artifactViewName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'left',
+    marginTop: 5,
+    marginLeft: 8,
+    marginRight: 10
+  },
+  artifactViewText: {
+    fontSize: 16,
+    fontWeight: 'normal',
+    textAlign: 'left',
+    marginLeft: 10,
+    marginTop: 8,
+    marginRight: 10,
+    marginBottom: 8
+  },
+  artifactViewDate: {
+    fontSize: 16,
+    fontWeight: 'normal',
+    textAlign: 'right',
+    marginTop: 5,
+    marginRight: 8,
+  },
+  artifactViewTags: {
+    fontSize: 16,
+    fontStyle: 'italic',
+    textAlign: 'left',
+    marginLeft: 10,
+    marginTop: 3,
+    marginRight: 10,
+    marginBottom: 8
   }
 });
 
@@ -51,24 +81,20 @@ class ArtifactView extends Component {
   };
 
   render() {
-    console.log('these are the props from ALV', this.props);
     return (
       <View style={styles.container}>
         <View style={styles.imageContainer}>
-          <Image source={{uri: this.props.base64}}/>
-        </View>
-        
-        <View>
-          <Text style={styles.listText}>{this.props.route.username}</Text>
+          <Image style={styles.artifactViewImage} source={{uri: this.props.base64}}/>
         </View>
         <View>
-          <Text style={styles.listText}>{this.props.route.date}</Text>
+          <Text style={styles.artifactViewName}>{this.props.route.username} <Text style={styles.artifactViewDate}>{this.props.route.date}</Text></Text>
         </View>
         <View>
-          <Text style={styles.listText}>{this.props.route.message}</Text>
+          <Text style={styles.artifactViewText}>{this.props.route.message}</Text>
         </View>
         <View>
-          <Text style={styles.listText}>{this.props.route.tags}</Text>
+          <Text style={styles.artifactViewTags}>Tags:</Text>
+          <Text style={styles.artifactViewTags}>{this.props.route.tags}</Text>
         </View>
       </View>
     );
