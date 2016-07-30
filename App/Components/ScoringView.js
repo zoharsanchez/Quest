7a6e0cbeaf5085c80757282738f3f78d0cc93b5b
Leah Loversky <ldloversky@gmail.com>
2016-07-29 23:15:48 -0700

    Style scoring view

--- App/Components/ScoringView.js
+++ App/Components/ScoringView.js
@@ -19,45 +19,17 @@ class ScoringView extends Component {
   componentDidMount() {
     var newTags = this.props.route.newTags;
     this.state.picScore = Math.pow(newTags.length, 2) * 10 + (newTags.length === 20 ? 1000 : 0) - (newTags.length === 0 ? 10 : 0);
-    console.log('picScore', this.state.picScore);
 
     var newScore = this.props.gameScore + this.state.picScore;
     var newCount = this.props.picCount + 1;
-    console.log('values created', newScore, newCount);
-    // this.props.updateGame(this.props.gameScore + this.state.picScore, this.state.picCount + 1);
     this.props.updateGame(newScore, newCount);
-    // console.log('after updateGame');
-
-    // this.props.userRef.ref('users/' + this.user.displayName.toLowerCase() + '/gameScore').set(newScore);
-    // this.props.userRef.ref('users/' + this.user.displayName.toLowerCase() + '/picCount').set(newCount);
-
-    // console.log('after db sets');
-
-    // this.props.userRef.ref('users/' + this.user.displayName.toLowerCase() + '/currentScore').once('value', (oldScore) => {
-    //   console.log('stored score', oldScore.val());
-    //   this.state.gameScore = oldScore.val() + this.state.picScore;
-    //   this.props.userRef.ref('users/' + this.user.displayName.toLowerCase() + '/currentScore').set(this.state.gameScore);
-    // });
-
-    // this.props.userRef.ref('users/' + this.user.displayName.toLowerCase() + '/gamePics').once('value', (pics) => {
-    //   console.log('game pics', pics.val());
-    //   this.state.gamePics = pics.val() + 1;
-    //   this.props.userRef.ref('users/' + this.user.displayName.toLowerCase() + '/gamePics').set(this.state.gamePics);
-    // });
   }
 
   componentWillReceiveProps(nextProps) {
     if (this.props.gameScore !== nextProps.gameScore || this.props.picCount !== nextProps.picCount) {
-      console.log('receiving props', nextProps.gameScore, nextProps.picCount);
+
       this.props.userRef.ref('users/' + this.user.displayName.toLowerCase() + '/gameScore').set(nextProps.gameScore);
       this.props.userRef.ref('users/' + this.user.displayName.toLowerCase() + '/picCount').set(nextProps.picCount);
-
-      console.log('after db sets');
-
-      // var allDone = _.reduce(this.props.currentTags, function(acc, tag) {
-      //   return acc && tag.done;
-      // }, true);
-      // console.log('allDone', allDone);
 
       if (_.every(this.props.currentTags, ['done', true])) {
         this.state.gameOver = 'You got all 20 tags!';
@@ -68,7 +40,7 @@ class ScoringView extends Component {
   }
 
   navToGameOver() {
-    console.log('navToGameOver', this.props);
+
     this.props.navigator.push({
       name: 'GameOverView',
       imagePath: this.props.path,
@@ -80,7 +52,7 @@ class ScoringView extends Component {
       let tagsObj = rawTags.val();
       let tags = Object.keys(tagsObj);
       let newTags = _.sampleSize(tags, 20);
-      console.log(newTags);
+
       let newState = _.map(newTags, (tag) => {return {tag: tag, done: false}; });
       this.props.changeTags(newState);
       this.props.updateGame(0, 0);
@@ -89,7 +61,7 @@ class ScoringView extends Component {
         gameScore: 0,
         picCount: 0
       });
-      console.log('started new game');
+
     });
   }
 
@@ -103,18 +75,30 @@ class ScoringView extends Component {
         </TouchableHighlight>
       </View>
     );
+
     return (
       <View style={styles.container}>
-        <View>
+
+        <View style={styles.imageContainer}>
+          <Image source={{uri: this.props.route.imagePath}}
+                 style= {styles.image}/>
+        </View>
+
+        <View style={styles.tagsContainer}>
+          {this.props.route.newTags.map((tag) =>
+            <View style={styles.tagContainer}>
+              <Text key={tag} style={styles.tagText}>{tag}</Text>
+            </View>
+          )}
+        </View>
+
+        <View style={styles.scoreContainer}>
           <Text>Scoring</Text>
           <Text>{'Pic Score: ' + this.state.picScore}</Text>
           <Text>{'Game Score: ' + this.props.gameScore}</Text>
           <Text>{'Pics Taken: ' + this.props.picCount}</Text>
-          <Image source={{uri: this.props.route.imagePath}}
-                 style= {styles.image}/>
-          <Text>New Tags</Text>
-          {this.props.route.newTags.map((tag) => <Text key={tag}>{tag}</Text>)}
         </View>
+
         <Text>{this.state.gameOver}</Text>
         {this.state.gameOver ? gameOverButton : null}
       </View>
--- App/Components/Styles/ScoringViewStyle.js
+++ App/Components/Styles/ScoringViewStyle.js
@@ -3,9 +3,40 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 65,
     flexDirection: 'column',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center'
+  },
+  scoreContainer: {
+    backgroundColor: 'blue',
+    flex: 0.2
+  },
+  imageContainer: {
+    backgroundColor: 'pink',
+    flex: 0.3
+  },
+  tagsContainer: {
+    paddingTop: 4,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    flexWrap: 'wrap'
+  },
+  tagContainer: {
+    backgroundColor: '#03a9f4',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+    borderRadius: 8,
+    marginTop: 4,
+    marginBottom: 4,
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  tagsText: {
+
   },
   image: {
     height: 150,
